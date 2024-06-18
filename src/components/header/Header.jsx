aa038caab6b1e5d4a0ed8df1913552fac9429ddb
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-06-18 10:23:56 +0530

    header section completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,116 @@
-import React from 'react'
-import './style.scss'
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
 
 function Header() {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[location])
+
+  const controlNavbar=()=>{
+    console.log(window.scrollY);
+    if(window.scrollY>200){
+      if(window.scrollY > lastScrollY && !mobileMenu){
+        setShow("hide")
+      }else{
+        setShow("show")
+    }
+  }
+  else{
+   setShow("top")
+    }
+    setLastScrollY(window.scrollY)
+  }
+
+  useEffect(()=>{
+    window.addEventListener("scroll",controlNavbar)
+
+    return ()=>{
+      window.removeEventListener("scroll",controlNavbar)
+    }
+  },[lastScrollY ])
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+    setTimeout(() => {
+      setShowSearch(false)
+    }, 1000);
+  };
+
+  const openSearch=()=>{
+    setMobileMenu(false);
+    setShowSearch(true)
+  }
+
+  const opneMobileMenu=()=>{
+    setMobileMenu(true);
+    setShowSearch(false)
+  }
+
+  const navigationHandler=(type)=>{
+    if(type==="movie"){
+      navigate("/explore/movie")
+    }
+    else{
+      navigate("/explore/tv")
+    }
+  }
+
   return (
-    <div>
-        Header
-    </div>
-  )
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="l ogo">
+          <img src={logo} alt="" />
+          </div>
+          <ul className="menuItems">
+            <li className="menuItem" onClick={()=> navigationHandler("movie")}>movie</li>
+            <li className="menuItem" onClick={()=> navigationHandler("tv")}>Tv shows</li>
+            <li className="menuItem">
+              <HiOutlineSearch onClick={openSearch} />
+            </li>
+          </ul>
+          <div className="mobileMenuItems">
+            <HiOutlineSearch onClick={openSearch}/>
+            {
+              mobileMenu ? <VscChromeClose onClick={()=>setMobileMenu(false)}/> :<SlMenu onClick={opneMobileMenu}/>
+            }
+          </div>
+       
+      </ContentWrapper>
+      {
+        showSearch && <div className="searchBar">
+        <ContentWrapper>
+        <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv shows..."
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <VscChromeClose onClick={()=>setShowSearch(false)}/>
+          </div>
+        </ContentWrapper>
+      </div>
+      }
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { useNavigate } from "react-router-dom";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 import useFetch from "../../../hooks/useFetch";
 import Img from "../../../components/lazyLoadImage/img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
@@ -10,12 +10,14 @@ function HeroBanner() {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
- const {url}=useSelector((state)=>state.home)
+  const { url } = useSelector((state) => state.home);
 
   const { data, loading } = useFetch("/movie/upcoming");
 
   useEffect(() => {
-    const bg =url.backdrop+ data?.results[Math.floor(Math.random() * 20)]?.backdrop_path;
+    const bg =
+      url.backdrop +
+      data?.results[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
   }, [data]);
 
@@ -28,20 +30,19 @@ function HeroBanner() {
   return (
     <div className="heroBanner">
       <div className="backdrop-img">
-          {
-            !loading && <div className="backdrop-img">
-              <Img src={background}/>
-            </div>
-          }
+        {!loading && (
+          <div className="backdrop-img">
+            <Img src={background} />
+          </div>
+        )}
       </div>
-      <div className="opacity-layer">
-
-      </div>
+      <div className="opacity-layer"></div>
       <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
           <span className="subTitile">
-            Millions of movies,TV shows and people to discover. Sahebrao jadhav.
+            Millions of movies,TV shows and people to discover. <br /> Sahebrao
+            jadhav's project.
           </span>
           <div className="searchInput">
             <input
