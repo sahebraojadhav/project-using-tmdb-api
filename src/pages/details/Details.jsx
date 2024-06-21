fa8451bee76e06dffd69e027bf376dab87011bfc
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-06-21 10:48:09 +0530

    create Top Cast tab

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,16 +3,18 @@ import { useParams } from 'react-router-dom'
 import "./style.scss"
 import useFetch from "../../hooks/useFetch"
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
 
 function Details() {
   const {mediaType,id}=useParams();
   const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
-  const{data:credits,loading:creditLoading}=useFetch(`/${mediaType}/${id}/credits`);
+  const{data:credits,loading:creditsLoading}=useFetch(`/${mediaType}/${id}/credits`);
 
 
   return (
     <div>
       <DetailsBanner video={data?.results[0]} crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditsLoading}/>
     </div>
   )
 }
